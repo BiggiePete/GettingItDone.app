5449e8953ed05e7264c9e38a397dfd843b845acb
biggiePete <peter.cross222@gmail.com>
2024-02-27 14:58:30 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container mx-auto h-full flex justify-center items-center">
+	<div class="fig">
+		<div class="card w-full p-4 m-4">
+			<div class="img-bg"></div>
+			<div class="card-header">
+				<span>Something to do:</span>
+				<input type="text" class="input" placeholder="thing to do" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.fig {
+		@apply flex relative flex-col;
+	}
+	.fig .tin,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-20;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[70px] transition-all;
+		animation:
+			pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
